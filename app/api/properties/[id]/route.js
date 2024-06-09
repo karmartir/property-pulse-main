a558160e1a245e2c09f8cd013e60f4223f582b54
garen <karmartir@gmail.com>
2024-06-08 23:17:59 -0400

    Delete function upgrade to remove images from Cloudinary too

--- app/api/properties/[id]/route.js
+++ app/api/properties/[id]/route.js
@@ -1,6 +1,10 @@
 import connectDB from '@/config/database';
 import Property from '@/models/Property';
 import { getSessionUser } from '@/utils/getSessionUser';
+import cloudinary from '@/config/cloudinary';
+
+// NOTE: here we need to send back a Content-Type: application/json response
+// header rather than a text/plain header.
 
 // GET /api/properties/:id
 export const GET = async (request, { params }) => {
@@ -11,9 +15,7 @@ export const GET = async (request, { params }) => {
 
     if (!property) return new Response('Property Not Found', { status: 404 });
 
-    return new Response(JSON.stringify(property), {
-      status: 200,
-    });
+    return Response.json(property);
   } catch (error) {
     console.log(error);
     return new Response('Something Went Wrong', { status: 500 });
@@ -21,7 +23,9 @@ export const GET = async (request, { params }) => {
 };
 
 // DELETE /api/properties/:id
-export const DELETE = async (request, { params }) => {
+// NOTE: Here we also need to delete images from Cloudinary
+
+export const DELETE = async (_, { params }) => {
   try {
     const propertyId = params.id;
 
@@ -45,6 +49,20 @@ export const DELETE = async (request, { params }) => {
       return new Response('Unauthorized', { status: 401 });
     }
 
+    // extract public id's from image url in DB
+    const publicIds = property.images.map((imageUrl) => {
+      const parts = imageUrl.split('/');
+      return parts.at(-1).split('.').at(0);
+    });
+
+    // Delete images from Cloudinary
+    if (publicIds.length > 0) {
+      for (let publicId of publicIds) {
+        await cloudinary.uploader.destroy('propertypulse/' + publicId);
+      }
+    }
+
+    // Proceed with property deletion
     await property.deleteOne();
 
     return new Response('Property Deleted', {
@@ -118,9 +136,7 @@ export const PUT = async (request, { params }) => {
     // Update property in database
     const updatedProperty = await Property.findByIdAndUpdate(id, propertyData);
 
-    return new Response(JSON.stringify(updatedProperty), {
-      status: 200,
-    });
+    return Response.json(updatedProperty);
   } catch (error) {
     console.log(error);
     return new Response('Failed to add property', { status: 500 });
